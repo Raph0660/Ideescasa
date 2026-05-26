356ea136d3a4915cac1caa4cc1355a44e03d6739
Raph0660 <raphael.geronimi1@gmail.com>
2026-05-26 16:27:18 +0200

    Update robots.js

--- app/robots.js
+++ app/robots.js
@@ -1,9 +1,26 @@
 export default function robots() {
   return {
-    rules: {
-      userAgent: '*',
-      allow: '/',
-    },
+    rules: [
+      {
+        userAgent: '*',
+        allow: '/',
+        disallow: [
+          '/recherche',
+          '/filtres',
+          '/*?sort=',
+          '/*?page=',
+          '/*?filter=',
+          '/*?ref=',
+          '/*?utm_*',
+          '/api/'
+        ],
+      },
+      {
+        // On bloque les robots d'IA pour préserver ton serveur et tes contenus
+        userAgent: ['GPTBot', 'CCBot', 'Google-Extended', 'Anthropic-AI'],
+        disallow: '/',
+      }
+    ],
     sitemap: 'https://www.ideescasa.fr/sitemap.xml',
   }
 }
